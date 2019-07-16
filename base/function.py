d67a06ddb37ba116598fad08ebe6ec82579529c3
yuzi <hayuzi@163.com>
2019-07-16 14:53:52 +0800

    format

--- base/function.py
+++ base/function.py
@@ -94,7 +94,6 @@ total = sumB(10, 20)
 print("函数外 : ", total)
 
 
-
 # 变量作用域
 # Python 中，程序的变量并不是在哪个位置都可以访问的，访问权限决定于这个变量是在哪里赋值的。
 # 变量的作用域决定了在哪一部分程序可以访问哪个特定的变量名称。Python的作用域一共有4种，分别是：
@@ -104,34 +103,43 @@ print("函数外 : ", total)
 # B （Built-in） 内置作用域（内置函数所在模块的范围）
 # 以 L –> E –> G –>B 的规则查找，即：在局部找不到，便会去局部外的局部找（例如闭包），再找不到就会去全局找，再者去内置中找。
 g_count = 0  # 全局作用域
+
+
 def outer():
     o_count = 1  # 闭包函数外的函数中
+
     def inner():
         i_count = 2  # 局部作用域
 
-# 内置作用域是通过一个名为 builtin 的标准模块来实现的，但是这个变量名自身并没有放入内置作用域内，所以必须导入这个文件才能够使用它。在Python3.0中，可以使用以下的代码来查看到底预定义了哪些变量:
+# 内置作用域是通过一个名为 builtin 的标准模块来实现的，但是这个变量名自身并没有放入内置作用域内，所以必须导入这个文件才能够使用它。
+# 在Python3.0中，可以使用以下的代码来查看到底预定义了哪些变量:
 # >>> import builtins
 # >>> dir(builtins)
 
 # Python 中只有模块（module），类（class）以及函数（def、lambda）才会引入新的作用域，
-# 其它的代码块（如 if/elif/else/、try/except、for/while等）是不会引入新的作用域的，也就是说这些语句内定义的变量，外部也可以访问.
+# 其它的代码块（如 if/elif/else/、try/except、for/while等）是不会引入新的作用域的，
+# 也就是说这些语句内定义的变量，外部也可以访问.
 
 # 全局变量和局部变量
 # 定义在函数内部的变量拥有一个局部作用域，定义在函数外的拥有全局作用域。
 # 局部变量只能在其被声明的函数内部访问，而全局变量可以在整个程序范围内访问。
 # 调用函数时，所有在函数内声明的变量名称都将被加入到作用域中。
 
-total = 0 # 这是一个全局变量
+
+total = 0  # 这是一个全局变量
 # 可写函数说明
-def sumC( arg1, arg2 ):
-    #返回2个参数的和."
-    total = arg1 + arg2 # total在这里是局部变量.
-    print ("函数内是局部变量 : ", total)
+
+
+def sumC(arg1, arg2):
+    # 返回2个参数的和."
+    total = arg1 + arg2  # total在这里是局部变量.
+    print("函数内是局部变量 : ", total)
     return total
- 
-#调用sum函数
-sumC( 1, 2 )
-print ("函数外是全局变量 : ", total)
+
+
+# 调用sum函数
+sumC(1, 2)
+print("函数外是全局变量 : ", total)
 # 以上实例输出结果：
 #   函数内是局部变量 :  3
 #   函数外是全局变量 :  0
@@ -141,21 +149,30 @@ print ("函数外是全局变量 : ", total)
 # 当内部作用域想修改外部作用域的变量时，就要用到global和nonlocal关键字了。
 # 以下实例修改全局变量 num：
 num = 1
+
+
 def fun1():
     global num  # 需要使用 global 关键字声明
-    print(num) 
+    print(num)
     num = 123
     print(num)
+
+
 fun1()
 print(num)
 
 # 如果要修改嵌套作用域（enclosing 作用域，外层非全局作用域）中的变量则需要 nonlocal 关键字了，如下实例：
+
+
 def outerfunc():
     num = 10
+
     def innerfunc():
         nonlocal num   # nonlocal关键字声明
         num = 100
         print(num)
     innerfunc()
     print(num)
+
+
 outerfunc()
